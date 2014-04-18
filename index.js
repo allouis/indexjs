0018d69690b6ed412fdf6c24d31abefa5cca8847
Fabien O'Carroll <fabien@allou.is>
2014-04-18 13:52:31 +0100

    move createModuleCreationFunction into its own module

--- index.js
+++ index.js
@@ -3,24 +3,7 @@ module.exports = indexjs;
 var fs = require('fs');
 var path = require('path');
 
-function createModuleCreationFunction (fn) {
-  return function createModuleObject (dirname) {
-    return function moduleObjectReduce (modules, filename) {
-      var parts = filename.split('.');
-      if (parts[0] !== 'index' && parts.pop() === 'js') {
-        fn(modules, require(path.join(dirname, filename)), parts);
-      }
-      return modules;
-    };
-  };
-}
-
-var createModuleArray = createModuleCreationFunction(function (modules, module) {
-  modules.push(module);
-});
-var createModuleObject = createModuleCreationFunction(function (modules, module, parts) {
-  modules[parts[0]] = module;
-});
+var createModuleCreationFunction = require('./lib/createModuleFunction');
 
 var outputFunctions = {
   array: createModuleArray,
