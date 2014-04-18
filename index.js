d8b0ede2a56f84f9e2e07a6d070b4cfd3a424fcd
Fabien O'Carroll <fabien@allou.is>
2014-04-18 13:37:53 +0100

    remove mass duplication of code

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ module.exports = indexjs;
 var fs = require('fs');
 var path = require('path');
 
+function createModuleCreationFunction (fn) {
+  return function createModuleObject (dirname) {
+    return function moduleObjectReduce (modules, filename) {
+      var parts = filename.split('.');
+      if (parts[0] === 'index' || parts.pop() !== 'js') {
+        return modules;
+      }
+      fn(modules, require(path.join(dirname, filename)), parts);
+      return modules;
+    };
+  };
+}
+
+var createModuleArray = createModuleCreationFunction(function (modules, module) {
+  modules.push(module);
+});
+var createModuleObject = createModuleCreationFunction(function (modules, module, parts) {
+  modules[parts[0]] = module;
+});
+
 var outputFunctions = {
   array: createModuleArray,
   object: createModuleObject
@@ -18,28 +38,4 @@ function indexjs (dirname, output) {
   }
   var createModuleOutput = outputFunctions[type];
   return fs.readdirSync(dirname).reduce(createModuleOutput(dirname), output);
-}
-
-function createModuleArray (dirname) {
-  return function moduleArrayReduce (modules, filename) {
-    var parts = filename.split('.');
-    if (parts[0] === 'index' || parts.pop() !== 'js') {
-      return modules;
-    }
-    var fullpath = path.join(dirname, filename);
-    modules.push(require(fullpath));
-    return modules;
-  };
-}
-
-function createModuleObject (dirname) {
-  return function moduleObjectReduce (modules, filename) {
-    var parts = filename.split('.');
-    if (parts[0] === 'index' || parts.pop() !== 'js') {
-      return modules;
-    }
-    var fullpath = path.join(dirname, filename);
-    modules[parts[0]] = require(fullpath);
-    return modules;
-  };
 }
