a875a5a0335b9f3fa10b1abdd4d201098da3f6d6
Fabien O'Carroll <fabien@allou.is>
2014-04-18 13:15:53 +0100

    add functionality to support object outputs

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ module.exports = indexjs;
 var fs = require('fs');
 var path = require('path');
 
-function indexjs (dirname, type) {
-  return fs.readdirSync(dirname).reduce(createModuleArray(dirname), []);
+var outputFunctions = {
+  array: createModuleArray,
+  object: createModuleObject
+};
+
+function indexjs (dirname, output) {
+  var createModuleOutput = outputFunctions[typeof output];
+  return fs.readdirSync(dirname).reduce(createModuleOutput(dirname), output);
 }
 
 function createModuleArray (dirname) {
@@ -15,6 +21,18 @@ function createModuleArray (dirname) {
     }
     var fullpath = path.join(dirname, filename);
     modules.push(require(fullpath));
+    return modules;
+  };
+}
+
+function createModuleObject (dirname) {
+  return function moduleObjectReduce (modules, filename) {
+    var parts = filename.split('.');
+    if (parts[0] === 'index' || parts.pop() !== 'js') {
+      return modules;
+    }
+    var fullpath = path.join(dirname, filename);
+    modules[parts[0]] = require(fullpath);
     return modules;
   };
 }
